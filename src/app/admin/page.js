04ba67ba95af60c626d3872ead306bd91d7aea2a
chris92vr <garofolichristian@gmail.com>
2023-02-23 13:44:12 +0100

    Integrated routes

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -6,11 +6,23 @@ function Page() {
   const { user } = useAuthContext();
   const router = useRouter();
 
-  React.useEffect(() => {
-    if (user == null) router.push('/');
-  }, [user]);
+  // if user is not logged in, redirect to login page
+  if (!user) {
+    router.push('/signin');
+  }
 
-  return <h1>Only logged in users can view this page</h1>;
+  return (
+    <div className="wrapper">
+      <div className="form-wrapper">
+        <h1 className="mt-60 mb-30">Admin</h1>
+        <div className="form">
+          <p>Admin page</p>
+          <p>You are logged in as {user.email}</p>
+          <button onClick={() => router.push('/')}>Go to home page</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Page;
