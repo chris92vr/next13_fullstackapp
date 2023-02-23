b6860c5716a7c5bd851c9b63305c1edadcbd785a
chris92vr <garofolichristian@gmail.com>
2023-02-23 13:36:39 +0100

    Implemented getAllDocuments function

--- src/firebase/firestore/getAllData.js
+++ src/firebase/firestore/getAllData.js
@@ -2,18 +2,20 @@ import firebase_app from '../config';
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 const db = getFirestore(firebase_app);
-const getAllDocuments = async (collectionName) => {
-  let result = null;
-  let error = null;
-
-  try {
-    const querySnapshot = await getDocs(collection(db, collectionName));
-    result = querySnapshot.docs.map((doc) => doc.data());
-  } catch (e) {
-    error = e;
-  }
-
-  return { result, error };
+const getAllDocuments = async (collectionName, user) => {
+  const data = [];
+  // get all documents in the collection by the user
+  const querySnapshot = await getDocs(collection(db, collectionName));
+  querySnapshot.forEach((doc) => {
+    if (doc.data().createdBy === user) {
+      data.push(doc.data());
+    } else {
+      console.log('No such document!');
+    }
+  });
+  console.log('user:', user);
+  console.log('data:', data);
+  return data;
 };
 
 export default getAllDocuments;
