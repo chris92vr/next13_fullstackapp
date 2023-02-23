a3f77fe87068bfb3d57232fef967bd93c633099b
chris92vr <garofolichristian@gmail.com>
2023-02-23 13:37:40 +0100

    Structured Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,91 +1,50 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+'use client';
+import React from 'react';
+import { useAuthContext } from '@/context/AuthContext';
+import { useRouter } from 'next/navigation';
 
-const inter = Inter({ subsets: ['latin'] })
+function Page() {
+  const { user } = useAuthContext();
+  const router = useRouter();
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  // if user is not logged in, show home page with message
+  if (!user) {
+    return (
+      <div className="wrapper">
+        <div className="form-wrapper">
+          <h1 className="mt-60 mb-30">home</h1>
+          <div className="form">
+            <p>You are not logged in</p>
+            <button onClick={() => router.push('/signin')}>Sign in</button>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+    );
+  } else {
+    return (
+      <>
+        <div className="wrapper">
+          <div className="form-wrapper">
+            <h1 className="mt-60 mb-30">home</h1>
+            <div className="form">
+              <p>You are logged in as {user.email}</p>
+              <button onClick={() => router.push('/admin')}>
+                Go to admin page
+              </button>
+              <p> create post</p>
+              <button onClick={() => router.push('/createPost')}>
+                Go to create post page
+              </button>
+              <p> view posts</p>
+              <button onClick={() => router.push('/viewPosts')}>
+                Go to view posts page
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </>
+    );
+  }
 }
+
+export default Page;
