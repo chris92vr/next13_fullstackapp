14b9ccf05da0bc324e1b156afe6f4ee4626d225a
chris92vr <garofolichristian@gmail.com>
2023-02-25 11:00:30 +0100

    Added getDocByID function

--- src/firebase/firestore/getData.js
+++ src/firebase/firestore/getData.js
@@ -2,17 +2,14 @@ import firebase_app from '../config';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 
 const db = getFirestore(firebase_app);
-export default async function getDoument(collection, id) {
-  let docRef = doc(db, collection, id);
-
-  let result = null;
-  let error = null;
-
-  try {
-    result = await getDoc(docRef);
-  } catch (e) {
-    error = e;
+const getDocById = async (collectionName, id) => {
+  const docRef = doc(db, collectionName, id);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    return docSnap.data();
+  } else {
+    console.log('No such document!');
   }
+};
 
-  return { result, error };
-}
+export default getDocById;
